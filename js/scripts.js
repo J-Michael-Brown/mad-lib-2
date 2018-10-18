39b63f0f64909ba695fdbdf74f0b32e1b62729a6
Micheal Brown and Jacey Tupa <jacey@>
2018-10-18 14:22:12 -0700

    Add check for filled out catagoties

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,45 @@ $(document).ready(function() {
     var exclamationInput = $("input#exclamation").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
+    $(".fill-out").hide();
 
+  if (person1Input){
     $(".person1").text(person1Input);
+  } else {
+    $("#person1fill").show();
+  }
+  if (person2Input){
     $(".person2").text(person2Input);
+  } else {
+    $("#person2fill").show();
+  }
+  if (animalInput){
     $(".animal").text(animalInput);
+  } else {
+    $("#animalfill").show();
+  }
+  if (exclamationInput){
     $(".exclamation").text(exclamationInput);
+  } else {
+    $("#exclamationfill").show();
+  }
+  if (verbInput){
     $(".verb").text(verbInput);
+  } else {
+    $("#verbfill").show();
+  }
+  if (nounInput){
     $(".noun").text(nounInput);
+  } else {
+    $("#nounfill").show();
+  }
 
+  if (!person1Input || !person2Input || !animalInput || !exclamationInput || !verbInput || !nounInput) {
+    $("#empty-fields").show();
+
+  } else {
     $("#story").show();
+  }
 
     event.preventDefault();
   });
